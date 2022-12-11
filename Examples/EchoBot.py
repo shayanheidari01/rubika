6883c583c0eccd005600627021d2722d2b29df26
Shayan Heidari <119259115+shayanheidari01@users.noreply.github.com>
2022-12-11 03:54:55 +0330

    Create EchoBot.py

---
+++ Examples/EchoBot.py
@@ -0,0 +1,15 @@
+from runpy import Client
+
+app = Client('auth')
+
+@app.handler
+async def EchoBot(bot, message):
+    if await message.of_user():
+        if await message.is_text:
+            await message.reply(
+                await message.text()
+            )
+
+
+# If a new message is received and the message is of text type
+Copies the textual content of that message and replies to the same message again
