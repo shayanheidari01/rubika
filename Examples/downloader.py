4e058bc779c19fe6d4c2252c5d33917d6c72b9cc
Shayan Heidari <119259115+shayanheidari01@users.noreply.github.com>
2023-01-19 23:29:30 +0330

    debug downloader

--- Examples/downloader.py
+++ Examples/downloader.py
@@ -1,24 +1,26 @@
-from rubpy import Client
+from rubpy import Client, Methods, Message
 
 app = Client(
     auth='auth-key',
-    account_guid='account-object-guid'
+    account_guid='account_guid',
 )
 
-@app.MessageUpdates
-async def Downloader(bot, message):
+@app.handler
+async def Downloader(bot: Methods, message: Message):
     if await message.of_group() and await message.is_text():
         text = await message.text()
-        if text.startswith('دانلود'):
-            message_info = await message.show()
-            message_info = message_info.get('message')
-            if 'reply_to_message_id' in message_info.keys():
-                print(await message.show())
-                await bot.download(
+        if text == 'دانلود':
+            message_options = await message.show()
+            message_options = message_options.get('message')
+            if 'reply_to_message_id' in message_options.keys():
+                check = await bot.download(
                     object_guid=await message.chat_id(),
-                    message_id=message_info.get('message_id'),
-                    save=True
+                    message_id=message_options.get('reply_to_message_id'),
+                    save=True,
                 )
-                await message.reply('فایل مورد نظر دانلود و ذخیره شد')
+                if check:
+                    await message.reply('فایل مورد نظر دانلود شد.')
+                else:
+                    await message.reply('خطایی رخ داد!')
             else:
-                await message.reply('لطفا روی یک فایل ریپلای بزنید')
+                await message.reply('لطفا روی یک فایل ریپلای بزنید!')
