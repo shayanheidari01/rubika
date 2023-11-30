b59694713aa73f84a5faefd1e1aee9d6510d76ef
Shayan Heidari <119259115+shayanheidari01@users.noreply.github.com>
2023-11-30 22:53:01 +0330

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup, find_packages
 
-requirements = ['aiohttp', 'pycryptodome', 'aiofiles']
+requirements = [
+    'aiohttp',
+    'pycryptodome',
+    'aiofiles',
+    'mutagen',
+    'websocket-client',
+    'requests',
+    'pydantic==1.10.12'
+]
 
-with open("README.md", encoding="UTF-8") as f:
+with open('README.md', encoding='UTF-8') as f:
     readme = f.read()
 
 setup(
     name = 'rubpy',
-    version = '6.0.4',
+    version = '6.4.9',
     author='Shayan Heidari',
     author_email = 'contact@shayanheidari.info',
     description = 'This is an unofficial library and fastest library for deploying robots on Rubika accounts.',
@@ -19,7 +27,7 @@ setup(
     packages = find_packages(),
     install_requires = requirements,
     extras_require={
-        'opencv-python': ['opencv-python']
+        'cv': ['opencv-python']
     },
     classifiers=[
         'Programming Language :: Python :: 3.7',
