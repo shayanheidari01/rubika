ab4a494798185c2aa73f15c2cabd1f703835db9e
Shayan Heidari <119259115+shayanheidari01@users.noreply.github.com>
2024-01-09 17:48:54 +0330

    Add files via upload

---
+++ Examples/rubino_dl.py
@@ -0,0 +1,29 @@
+from rubpy import Client, filters, Rubino
+from rubpy.types import Updates
+from httpx import AsyncClient
+
+bot = Client('bot')
+rubino = Rubino(client=bot)
+httpx = AsyncClient()
+
+@bot.on_message_updates(filters.text)
+async def updates(update: Updates):
+    text: str = update.raw_text
+
+    if text.startswith('https://rubika.ir/post/'):
+        post = await rubino.get_post_by_share_link(text)
+        if post.has_access:
+            post = post.post
+            if post.file_type == 'Video':
+                #await update.reaction(1)
+                await bot.send_video(update.object_guid,
+                                     video=post.full_file_url,
+                                     reply_to_message_id=update.message_id)
+            elif post.file_type == 'Picture':
+                #await update.send_activity('Uploading')
+                #await update.reaction(1)
+                await bot.send_photo(update.object_guid,
+                                     photo=post.full_file_url,
+                                     reply_to_message_id=update.message_id)
+
+bot.run()
