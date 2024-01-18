2dfeb57f4d4be3fc83b0fe85b95488e006c09ac1
Arvin Radmehr <74110031+staliox@users.noreply.github.com>
2024-01-19 00:40:20 +0330

    Update group_preview_by_join_link.py
    fix link hash parameter

--- rubpy/methods/groups/group_preview_by_join_link.py
+++ rubpy/methods/groups/group_preview_by_join_link.py
@@ -1,12 +1,16 @@
+import rubpy
+
 class GroupPreviewByJoinLink:
     async def group_preview_by_join_link(
-            self,
+            self: "rubpy.Client",
             link: str,
     ):
         if '/' in link:
             link = link.split('/')[-1]
 
-        return await self.builder('groupPreviewByJoinLink',
-                                  input={
-                                      'link': link,
-                                  })
+        return await self.builder(
+            name='groupPreviewByJoinLink',
+            input={
+                'hash_link': link
+            }
+        )
