28b82a5850eebd601b384a8a10209dd157d6d64d
Shayan Heidari <119259115+shayanheidari01@users.noreply.github.com>
2024-02-16 16:26:22 +0330

    Update image_color.py

--- examples/image_color.py
+++ examples/image_color.py
@@ -3,17 +3,17 @@ from rubpy.types import Updates
 from PIL import Image
 from io import BytesIO
 
-bot = Client('ImageColor')
+bot = Client('bot')
 
-@bot.on_message_updates(filters.is_group, filters.Commands('rgb'))
+@bot.on_message_updates(filters.is_group, filters.Commands(['rgb', 'RGB']))
 def make_image(update: Updates):
     color: str = update.command[-1]
     color = tuple(int(color[i:i+2], 16) for i in (1, 3, 5))
-    image = Image.new('RGB', (100, 100))
+    image = Image.new('RGB', (10, 10))
     buffer = BytesIO()
 
-    for y in range(100):
-        for x in range(100):
+    for y in range(10):
+        for x in range(10):
             image.putpixel((x, y), color)
 
     image.save(buffer, 'PNG')
