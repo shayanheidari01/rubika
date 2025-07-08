958f608b65d6fbc5ef3b2e2511c2d0ed624cfb2a
mahdoosh1 <mahdiyadiilavi1@gmail.com>
2025-07-08 22:42:15 +0330

    Update to 6.8.8

--- rubpy/methods/users/set_block_user.py
+++ rubpy/methods/users/set_block_user.py
@@ -1,10 +1,13 @@
+import rubpy
+from typing import Literal
+
 class SetBlockUser:
     async def set_block_user(
             self,
             user_guid: str,
-            action: str = 'Block',
+            action: Literal['Block', 'Unblock'] = 'Block',
     ):
-        if not action in ():
+        if not action in ('Block', 'Unblock'):
             raise ValueError('`action` argument can only be `Block` or `Unblock`.')
 
         return await self.builder('setBlockUser',
