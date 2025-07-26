f510c6a6b87b64b9ca0de141653d375a6748f7d5
Shayan Heidari <119259115+shayanheidari01@users.noreply.github.com>
2025-07-26 13:05:15 +0330

    Update echo_bot.py

--- examples/echo_bot.py
+++ examples/echo_bot.py
@@ -1,12 +1,13 @@
 from rubpy import BotClient
 from rubpy.bot import filters
+from rubpy.bot.models import Update
 
 app = BotClient("bot-token")
 
 
-@app.on_update(filters.text & filters.private)
-async def echo(client, update):
+@app.on_update(filters.text, filters.private)
+async def echo(client, update: Update):
     await update.reply(update.new_message.text)
 
 
-app.run()  # Automatically start()
+app.run() # Automatically start()
